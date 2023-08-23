bd288296e2f67d41864c79772d180bd128e8ea57
t-ryles <tajhakeem.ryles@gmail.com>
2023-08-22 21:48:49 -0500

    Finished lesson 1 - 5 -Starting a Compose project -Updating UI -Reusing composable -Creating columns and rows

--- app/src/main/java/com/example/composebasics_workshop/MainActivity.kt
+++ app/src/main/java/com/example/composebasics_workshop/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.composebasics_workshop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebasics_workshop.ui.theme.ComposeBasicsWorkshopTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicsWorkshopTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxWidth())
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MyApp(modifier: Modifier = Modifier,
+                  names: List<String> = listOf("World", "Compose")) {
+//    Surface (
+//        modifier = modifier,
+//        color = MaterialTheme.colorScheme.background
+//    ) {
+//        Greeting("Android")
+//    }
+    //vertical is added padding to the top and bottom of the column
+    Column(modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+//Composable fun. is just a reg. fun. with @Composable
+//which allow it to call another composable fun within it. [Text]
+fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        //Vertical = space between rows, horizontal = sides
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        //fillMaxWidth = color to fill space, padding for space around text
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)){
+            Text(text = "Hello", )
+            Text(text = "$name!")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeBasicsWorkshopTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
